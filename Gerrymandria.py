8dc7a6651dab687133cd6eac29641ebe0b9720e9
ngan-tran <trann@ripon.edu>
2017-06-15 15:10:11 -0500

    Updated 06/15/2017
    We can create districts and states now!

--- Gerrymandria.py
+++ Gerrymandria.py
@@ -17,8 +17,6 @@ class CBlock:
         self.population = 100
         self.shape = shape
 
-   
-
 class District:
     def __init__(self, name, cblocks):
         self.name = name
@@ -26,26 +24,28 @@ class District:
         self.shape = cascaded_union([cblock.shape for cblock in cblocks])
 
 class State:
-    def __init__(self, districts, all_cblocks):
+    def __init__(self, name, districts, all_cblocks):
+        self.name = name 
         self.districts = districts
+        self.shape = cascaded_union([district.shape for district in districts])
         self.all_cblocks = all_cblocks
         
+def make_cblocks():        
+    cblock_array = []
+    for i in range(8):
+        row = []
+        for j in range(8):
+            dems = 101
+            while dems < 0 or dems > 100:         
+                dems = random.gauss(50, 10) 
+                reps = 100 - dems
+                shape = Polygon([(i,j), (i+1,j), (i+1,j+1), (i,j+1)])
+                row.append(CBlock(dems, reps, 100, shape))
+        cblock_array.append(row)
+    return cblock_array
 
-cblock_array = []
-for i in range(8):
-    row = []
-    for j in range(8):
-        dems = 101
-        while dems < 0 or dems > 100:         
-            dems = random.gauss(50, 10) 
-            reps = 100 - dems
-            shape = Polygon([(i,j), (i+1,j), (i+1,j+1), (i,j+1)])
-            row.append(CBlock(dems, reps, 100, shape))
-    cblock_array.append(row)
-#print (cblock_array[0][3].shape.bounds)
 
-
-def make_dist(min_i, max_i, min_j, max_j, name):
+def make_dist(min_i, max_i, min_j, max_j, name, cblock_array):
     cblock_set = set()
     for i in range(min_i, max_i):
         for j in range(min_j, max_j):
@@ -53,11 +53,34 @@ def make_dist(min_i, max_i, min_j, max_j, name):
     district = District(name, cblock_set)
     return district
 
-district = make_dist(0,4,0,4, "District 1")
-print(district.shape.centroid)
-for cblock in district.cblocks:
-    print(cblock.dems)
+def make_state(name):
+    all_cblocks = make_cblocks()
+    district_set = set()
+    district_num = 1
+    for i in range(0,8,4):
+        for j in range(0,8,4):
+            district = make_dist (i, i+4, j, j+4, "{0}-{1}".format(name, district_num), all_cblocks)
+            district_set.add(district)
+            district_num += 1
+    state = State(name, district_set, all_cblocks)
+    return state 
 
+state_names = "ABCDEFGHIJKLMNOP"
+state_set = {make_state(name) for name in state_names}
+
+#print (state_set.pop().shape.area)
+#for state in state_set:
+#    print (state.districts.pop().name)
+#state_A = make_state("A")
+#print(state_A.districts.pop().name)
+#district = make_dist(0,4,0,4, "District 1")
+#print(district.shape.centroid)
+#for cblock in district.cblocks:
+#    print(cblock.dems)
+
+
+#x,y = p.exterior.xy
+#plt.plot(x,y,...)
 
                 
         
