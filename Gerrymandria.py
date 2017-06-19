dd7fd9446f3df6ae2e3d5c9ef01dafd1a4db4f7f
bartzb <bartzb@ripon.edu>
2017-06-19 09:18:07 -0500

    Some statistical added
    added demographic breakdown function - still want to check over again for bugs

--- Gerrymandria.py
+++ Gerrymandria.py
@@ -67,6 +67,37 @@ def make_state(name):
 
 state_names = "ABCDEFGHIJKLMNOP"
 state_set = {make_state(name) for name in state_names}
+All_district = []
+for state in state_set:
+     for district in state.districts:
+          All_district.append(district)
+
+def demographic_breakdown(state):    #find percentages of Democrats&Republicans in a given state
+    dems_list = []
+    reps_list = []
+    for i in range(len(All_district)):
+        if All_district[i].name[:1] == state:   #look at only districts in given state
+            for cblock in All_district[i].cblocks:
+                dems_list.append(cblock.dems)    #make list of number of Democrats in state's cblocks
+                reps_list.append(cblock.reps)    #make list of number of Republicans in state's cblocks
+    total_dem = sum(dems_list)
+    total_rep = sum(reps_list)
+    total_pop = total_dem + total_rep
+    total_percent_dem = total_dem / total_pop        #percent of votes cast for Democrat in given state
+    total_percent_rep = total_rep / total_pop        #percent of votes cast for Republican in given state                                   
+    return total_percent_dem, total_percent_rep
+
+total_percent_dem = demographic_breakdown(state)[0]
+total_percent_rep = demographic_breakdown(state)[1]
+
+New_list = []
+for dist in All_district:       #creates new list of districts to make up random 
+    if dist.name[:1] != state:  #list of districts that discludes districts from given state   
+        New_list.append(dist) 
+
+number_samples = 100
+
+
 
 #print (state_set.pop().shape.area)
 #for state in state_set:
