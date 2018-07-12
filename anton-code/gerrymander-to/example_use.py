44f81373df6a72a4287adaacb9293510db4ce6ad
McKenzie-Lamb <lambm@ripon.edu>
2018-07-12 14:04:43 -0500

    Committing in order to switch branches

--- anton-code/gerrymander-to/example_use.py
+++ anton-code/gerrymander-to/example_use.py
@@ -1,11 +1,11 @@
 import sys
 sys.path.append('./src/')
 
-import two_test_gerrymander_to
+import two_test_gerrymander_to as ttgt
 
 state_name = 'WI'     # used to determine total number of districts, as well as some other parameters
 state_dem_ratio = .5  # overall ratio of democratic votes
-target_dem_seats = 5  # target number of democratic seats
+target_dem_seats = 1  # target number of democratic seats
 max_tries = 100       # how many attempts to run before giving up
 
 # optional parameter(s)
@@ -13,9 +13,12 @@ max_tries = 100       # how many attempts to run before giving up
 
 # returns sorted list of ratios
 print ('Gerrymandering', state_name, 'to', target_dem_seats, 'Democarat seats')
-print (two_test_gerrymander_to.TwoTestGerrymanderTo(state_name, state_dem_ratio, target_dem_seats, max_tries))
+try:
+    print (ttgt.TwoTestGerrymanderTo(state_name, state_dem_ratio, target_dem_seats, max_tries))
+except:
+    print('Failed.')
 
 
 
 # rough range of seats for which the function above would find a valid distribution
-print(state_name, 'valid range of seats', two_test_gerrymander_to.TwoTestGetRange(state_name), '(approximate)')
+print(state_name, 'valid range of seats', ttgt.TwoTestGetRange(state_name), '(approximate)')
