283f6842407ea024961c1bf52d4b59e274e91c5d
McKenzie-Lamb <lambm@ripon.edu>
2018-08-03 13:47:20 -0500

    Commit to avoid conflicts

--- anton-code/gerrymander-to/example_use.py
+++ anton-code/gerrymander-to/example_use.py
@@ -5,14 +5,14 @@ import two_test_gerrymander_to as ttgt
 
 state_name = 'WI'     # used to determine total number of districts, as well as some other parameters
 state_dem_ratio = .5  # overall ratio of democratic votes
-target_dem_seats = 1  # target number of democratic seats
+target_dem_seats = 7  # target number of democratic seats
 max_tries = 100       # how many attempts to run before giving up
 
 # optional parameter(s)
 # quiet = True for no output
 
 # returns sorted list of ratios
-print ('Gerrymandering', state_name, 'to', target_dem_seats, 'Democarat seats')
+print ('Gerrymandering', state_name, 'to', target_dem_seats, 'Democrat seats')
 try:
     print (ttgt.TwoTestGerrymanderTo(state_name, state_dem_ratio, target_dem_seats, max_tries))
 except:
