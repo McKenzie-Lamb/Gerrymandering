209969e1720629b68802b8ce7e52923dc1bab3e5
MohammadNNB <43178365+MohammadNNB@users.noreply.github.com>
2018-09-24 17:28:15 -0500

    new wi graph with areas included and updated graph creat

--- abel-network-files/graph_creat.py
+++ abel-network-files/graph_creat.py
@@ -9,10 +9,14 @@ import fiona
 import networkx as nx
 from shapely.geometry import Polygon
 from shapely.geometry import Point
+import shapely.ops as ops
 import matplotlib.pyplot as plt
 import random
 import math
- 
+import pprint
+import pyproj
+from functools import partial
+
 
 def is_adjacent(shape1, shape2):
     # Checks for adjacency between two shapes
@@ -24,13 +28,26 @@ def is_adjacent(shape1, shape2):
 def is_adjacent_point_in(shape1, shape2):
     return shape1.touches(shape2)
 
+def get_area(geom):
+    geom_area = ops.transform(
+    partial(
+        pyproj.transform,
+        pyproj.Proj(init='EPSG:4326'),
+        pyproj.Proj(
+            proj='aea',
+            lat1=geom.bounds[1],
+            lat2=geom.bounds[3])),
+    geom)
+    return geom.area * 10000
+
 
 def get_poly(rings):
-    # Creates a shape object using the coordinates 
+    # Creates a shape object using the coordinates
     # provided by the .shp file
     # Inputs: A .shp ring
     # Output: the Polygon object
     tracts = []
+    area = 0
     for points in rings:
         ptlst = list(points)
         #In case there are not enough points to create a polygon
@@ -38,8 +55,10 @@ def get_poly(rings):
             print("BAD!")
             continue
         poly = Polygon(ptlst)
+        area += get_area(poly)
         tracts += [poly]
-    return tracts
+
+    return tracts, area
 
 
 def get_color(feat):
@@ -90,7 +109,7 @@ def get_position(tracts):
     new_max = 100
     new_min = 0
     new_range = new_max - new_min
-    
+
     centerx = 0
     centery = 0
     for i in tracts:
@@ -100,7 +119,7 @@ def get_position(tracts):
         #centery += (((y - old_min_y) * new_range)/old_range_y)
         centerx += x
         centery += y
-        
+
     return centerx/len(tracts), centery/len(tracts)
 
 
@@ -115,7 +134,7 @@ def separate_properties(properties, year):
             dem_share = .5
         dem = math.floor(pop*dem_share)
         rep = pop - dem
-    
+
     if year == 17:
         pop = int(properties['USHTOT16'])
         dem = int(properties['USHDEM16'])
@@ -126,7 +145,7 @@ def separate_properties(properties, year):
             dem_share = .5
         dem = math.floor(pop*dem_share)
         rep = pop - dem
-        
+
     return [pop,dem,rep]
 
 
@@ -154,58 +173,60 @@ def create_graph(daShapefile, rm_discontiguos = False, get_small_graph = False):
     '''
     Cretaes the graph
     param rm_discontiguos: True to delete all discontiguos components
-    param get_small_graph: 
+    param get_small_graph:
     '''
     with fiona.open(daShapefile[0]) as shapes:
         # Main for loop that reads the file, creates the graph,
         # add nodes, and edges
         graph = nx.Graph(directed=False)
         count = 0
-        
+        pprint.pprint(shapes[1])
+        total_area = 0
         print("Creating Graph...")
         for feat in shapes:
             properties = feat['properties']
             tract_id = feat['id']
             #color_n = get_color(feat)
             rlist = get_rlist(feat)
-            
+
             tracts = []
-            
+            area = 0
+
             for rings in rlist:
-                tracts += get_poly(rings)
-            
+                tracts += get_poly(rings)[0]
+                area += get_poly(rings)[1]
             properties = separate_properties(properties, daShapefile[1])
-            
+
             positionx, positiony = get_position(tracts)
-            graph.add_node(int(tract_id), polygon=tracts, pop=properties[0], rep=properties[1], dem=properties[2], pos=(positionx, positiony))
+            graph.add_node(int(tract_id), polygon=tracts, pop=properties[0], rep=properties[1], dem=properties[2], pos=(positionx, positiony), area=area)
             count += 1
+            total_area += area
             latest_node = list(graph.nodes(data=False))[-1]
             for n in list(graph.nodes())[:count-1]:
                 for i in graph.node[n]['polygon']:
-                    for j in graph.node[latest_node]['polygon']:       
+                    for j in graph.node[latest_node]['polygon']:
                         if is_adjacent(i, j):
                             graph.add_edge(n,latest_node)
-    
+
     # Changes positions from node attribute to graph attribute
     positions = positions_to_graph(graph)
     graph.graph['positions'] = positions
-    
+
     if rm_discontiguos:
         graph = clean_graph(graph)
-        
+
     if get_small_graph:
         small_graph = nx.ego_graph(graph, random.randint(1, len(graph)), radius=15)
         graph = small_graph
-    
+    print(total_area)
     return graph
 
-
-# Files path                            
-daShapefile14 = (r"E:\Projects\Gerrymandering\Gerrymandering\Drawing Wisconsin by Ward\Wards_fall_2014.shape\Wards_Final_Geo_111312_2014_ED.shp", 14)
-daShapefile17 = (r"E:\Projects\Gerrymandering\Gerrymandering\abel-network-files\data\20122020_WI_Election_Data_with_2017_Wards\2012_2020_Election_Data_with_2017_Wards.shp", 17)
+# Files path
+daShapefile14 = (r"/home/abel/School/Gerrymandering/Gerrymandering-master/abel-network-files/data/20122020_WI_Election_Data_with_2017_Wards/2012_2020_Election_Data_with_2017_Wards.shp", 17)
+#daShapefile17 = (r"E:\Projects\Gerrymandering\Gerrymandering\abel-network-files\data\20122020_WI_Election_Data_with_2017_Wards\2012_2020_Election_Data_with_2017_Wards.shp", 17)
 
 #Create the graph, clean and add the position attribute to the whole graph
-graph = create_graph(daShapefile17, rm_discontiguos = True)
+graph = create_graph(daShapefile14, rm_discontiguos = True)
 
 
 nx.draw(graph, graph.graph['positions'], node_size=(10))
